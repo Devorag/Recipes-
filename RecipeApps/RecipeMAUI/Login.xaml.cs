3d8af085a73914bfd1fbf776c8efbcca505cee3f
Devorag <141979085+Devorag@users.noreply.github.com>
2024-08-07 14:33:23 -0400

    following 32 video

---
+++ RecipeApps/RecipeMAUI/Login.xaml.cs
@@ -0,0 +1,19 @@
+namespace RecipeMAUI;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void Loginbtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Cancelbtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
