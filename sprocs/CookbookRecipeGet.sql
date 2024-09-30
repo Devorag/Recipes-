82c3502f081145d339f5e961f270d4c82d4ee1c0
Devorag <141979085+Devorag@users.noreply.github.com>
2024-09-30 11:07:25 -0400

    new work

--- sprocs/CookbookRecipeGet.sql
+++ sprocs/CookbookRecipeGet.sql
@@ -2,6 +2,7 @@ CREATE OR ALTER PROCEDURE dbo.CookbookRecipeGet(
     @CookbookRecipeId INT = 0,
     @CookbookId INT = 0,
     @All BIT = 0, 
+    @IncludeBlank bit = 0,
     @Message VARCHAR(500) = '' OUTPUT
 )
 AS 
@@ -11,11 +12,13 @@ BEGIN
     SELECT @All = ISNULL(@All, 0), 
            @CookbookRecipeId = ISNULL(@CookbookRecipeId, 0), 
            @CookbookId = ISNULL(@CookbookId, 0);
-    SELECT cr.CookbookRecipeId, cr.CookbookId, cr.RecipeId, cr.RecipeSequence
+    SELECT cr.CookbookRecipeId, cr.CookbookId, cr.RecipeId, cr.RecipeSequence, r.RecipeName
     FROM CookbookRecipe cr 
+    left join recipe r 
+    on r.RecipeId = cr.RecipeId
     WHERE (@All = 1) OR 
           (cr.CookbookRecipeId = @CookbookRecipeId) OR 
-          (cr.CookbookId = @CookbookId)
+          (cr.CookbookId = @CookbookId) 
     ORDER BY cr.recipeSequence 
 
     RETURN @return;
